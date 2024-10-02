490944097170ea9cb2672cb36c7d395589118626
Abeer <75672439+Abeer-Alsafran-ku@users.noreply.github.com>
2024-10-02 13:36:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("This is a Private chat application that allow communication through a chat room that is private and it can have only 2 members and it can be access throgh encrypted key only! 
+         The room name is created dynamically.
+         ")
